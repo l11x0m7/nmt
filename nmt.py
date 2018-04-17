79e5aeb9c4b46e22f49e154e2019e67d289405b7
l11x0m7 <l11x0m7@bupt.edu.cn>
2018-04-17 11:03:21 -0400

    debug nmt.py

--- nmt.py
+++ nmt.py
@@ -6,12 +6,34 @@ from tensorflow.python.layers import core as layers_core
 import numpy as np
 import cPickle as pkl
 
+import nltk
+from sklearn.model_selection import train_test_split
+
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
+
+batch_size = 32
+num_epochs = 20
+lr = 1e-3
+dropout = 0.2
+
+src_max_vocab_size = 60000
+tgt_max_vocab_size = 5000
+embedding_size = 128
+hidden_size = 128
+src_max_seq_len = 40
+tgt_max_seq_len = 40
+tgt_start_id = 2 # <S>
+tgt_end_id = 0 # <PAD>
+max_gradient_norm = 5
+maximum_iterations = 40
+cf = tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)
+cf.gpu_options.per_process_gpu_memory_fraction = 0.4
 
 
 def transform(data, word2id):
@@ -49,6 +71,28 @@ class Iterator(object):
         y_out_len = y_out_len.astype(np.int32)
         return x_part, y_out_part, y_in_part, x_len, y_in_len, y_out_len
 
+    def next(self, batch_size):
+        l = 0
+        while l < self.sample_num:
+            r = min(l + batch_size, self.sample_num)
+            batch_size = r - l
+            x_part = self.x[l:r]
+            y_out_part = self.y[l:r]
+            x_len = np.sum((x_part > 0), axis=1)
+            y_in_part = np.concatenate((np.ones((batch_size, 1)) * 2, y_out_part[:,:-1]), axis=-1)
+            max_y_dim = self.y.shape[1]
+            y_out_len = np.sum((y_out_part > 0), axis=1) + 1
+            y_out_len = np.min(np.concatenate([np.ones((batch_size, 1)) * max_y_dim, y_out_len.reshape(-1, 1)], axis=-1), axis=-1)
+            y_in_len = np.sum((y_in_part > 0), axis=1) + 1
+            y_in_len = np.min(np.concatenate([np.ones((batch_size, 1)) * max_y_dim, y_in_len.reshape(-1, 1)], axis=-1), axis=-1)
+            x_len = x_len.astype(np.int32)
+            y_in_part = y_in_part.astype(np.int32)
+            y_in_len = y_in_len.astype(np.int32)
+            y_out_len = y_out_len.astype(np.int32)
+            l += batch_size
+            yield x_part, y_out_part, y_in_part, x_len, y_in_len, y_out_len
+
+
 
 class NMTModel(object):
     def __init__(self, 
@@ -64,7 +108,7 @@ class NMTModel(object):
                  maximum_iterations=None
                  ):
         self.initializer = tf.random_uniform_initializer(
-        -0.01, 0.01)
+        -0.05, 0.05)
         self.src_max_vocab_size = src_max_vocab_size
         self.tgt_max_vocab_size = tgt_max_vocab_size
         self.embedding_size = embedding_size
@@ -150,6 +194,7 @@ class NMTModel(object):
             # Dynamic decoding
             outputs, _, _ = tf.contrib.seq2seq.dynamic_decode(decoder)
             self.logits = outputs.rnn_output
+            self.pred = tf.argmax(self.logits, axis=2)
 
         with tf.variable_scope('dynamic_decode', reuse=True):
             # Helper
@@ -187,35 +232,31 @@ class NMTModel(object):
             zip(clipped_gradients, params), global_step=self.global_step)
 
 
-def main():
-    batch_size = 32
-    num_epochs = 20
-    iter_num = 20000
-    lr = 1e-3
-    dropout = 0.2
+def bleu(refs, hyps):
+    refs = [[[_ for _ in ref if _ > 0]] for ref in refs]
+    hyps = [[_ for _ in hyp if _ > 0] for hyp in hyps]
+    return nltk.translate.bleu_score.corpus_bleu(refs, hyps)
 
-    src_max_vocab_size = 60000
-    tgt_max_vocab_size = 5000
-    embedding_size = 128
-    hidden_size = 128
-    src_max_seq_len = 40
-    tgt_max_seq_len = 40
-    tgt_start_id = 2 # <S>
-    tgt_end_id = 0 # <PAD>
-    max_gradient_norm = 5
-    maximum_iterations = 40
-    cf = tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)
-    cf.gpu_options.per_process_gpu_memory_fraction = 0.4
-
+def train():
     # load data and dictionary
-    with open('data/preprocess/demo_vocab_dict.pkl') as fr:
+    with open('data/preprocess/vocab_dict.pkl') as fr:
         en_word2id, en_id2word, ch_word2id, ch_id2word, \
         train_en_corpus, train_ch_corpus, test_en_corpus, test_ch_corpus = pkl.load(fr)
 
     train_en_corpus = padding(train_en_corpus, src_max_seq_len)
     train_ch_corpus = padding(train_ch_corpus, tgt_max_seq_len)
 
+    iter_num = train_en_corpus.shape[0] // batch_size + 1
+
+    # truncate the vocabrary
+    # for the words exceeded te vocab size, we set it as 1(<UNK>)
+    train_en_corpus[train_en_corpus >= src_max_vocab_size] = 1
+    train_ch_corpus[train_ch_corpus >= tgt_max_vocab_size] = 1
+
+    train_en_corpus, eval_en_corpus, train_ch_corpus, eval_ch_corpus = train_test_split(train_en_corpus, train_ch_corpus, test_size=0.1, )
+
     data_iterator = Iterator(train_en_corpus, train_ch_corpus)
+    eval_data_iterator = Iterator(eval_en_corpus, eval_ch_corpus)
 
     with tf.Session(config=cf) as sess:
         model = NMTModel(src_max_vocab_size=src_max_vocab_size, 
@@ -229,6 +270,7 @@ def main():
                          max_gradient_norm=max_gradient_norm,
                          maximum_iterations=maximum_iterations)
         sess.run(tf.global_variables_initializer())
+        saver = tf.train.Saver()
         for epoch in xrange(num_epochs):
             for iter_n in xrange(iter_num):
                 X, Y_out, Y_in, X_len, Y_in_len, Y_out_len = data_iterator.next_batch(batch_size)
@@ -238,7 +280,7 @@ def main():
                 # print(X_len.shape)
                 # print(Y_in_len.shape)
                 # print(Y_out_len.shape)
-                loss, _ = sess.run([model.loss_op, model.train_op], 
+                loss, _, global_step = sess.run([model.loss_op, model.train_op, model.global_step], 
                     feed_dict={ model.X:X,
                                 model.Y_out:Y_out,
                                 model.Y_in:Y_in, 
@@ -247,11 +289,54 @@ def main():
                                 model.Y_out_len:Y_out_len,
                                 model.lr:lr,
                                 model.dropout:dropout})
-                print('Loss:{}'.format(loss))
+                if iter_n % 1000 == 0:
+                    print('train loss:{}'.format(loss))
+                    evaluate(model, sess, eval_data_iterator)
+            saver.save(sess,'model/my_model', global_step=global_step)
+
+
+def evaluate(model, sess, data_iterator):
+    translations = []
+    refs = []
+    losses = []
+    for X, Y_out, Y_in, X_len, Y_in_len, Y_out_len in data_iterator.next(batch_size):
+        loss, translation = sess.run([model.loss_op, model.translations], 
+                        feed_dict={ model.X:X,
+                                    model.Y_in:Y_in,
+                                    model.Y_out:Y_out,
+                                    model.X_len:X_len,
+                                    model.Y_in_len:Y_in_len,
+                                    model.Y_out_len:Y_out_len,
+                                    model.lr:lr,
+                                    model.dropout:0.})
+        translations.append(translation)
+        refs.append(Y_out)
+        losses.append(loss)
+    translations = np.concatenate(translations, axis=0)
+    refs = np.concatenate(refs, axis=0)
+    bleu_score = bleu(refs, translations)
+    print('bleu score:{}, loss:{}'.format(bleu_score, np.mean(loss)))
+
+
+def predict(model, sess, X):
+    # X -> (src_max_seq_len, ) or (batch, sec_max_seq_len, )
+    if len(X.shape) == 1:
+        X = np.expand_dims(X, axis=0)
+    X_len = np.sum((X > 0), axis=1)
+    translations = sess.run(model.translations, 
+                        feed_dict={ model.X:X,
+                                    model.Y_out:Y_out,
+                                    model.Y_in:[[]], 
+                                    model.X_len:X_len,
+                                    model.Y_in_len:[],
+                                    model.Y_out_len:[],
+                                    model.lr:lr,
+                                    model.dropout:0.})
+    return translations
 
 
 if __name__ == '__main__':
-    main()
+    train()
 
 
 
